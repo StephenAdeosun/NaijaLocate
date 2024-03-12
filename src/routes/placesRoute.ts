9d8d7879b7fe03f642095a3802d9f1eaef45eccd
StephenAdeosun <stephenadeosun411@gmail.com>
2024-03-12 07:17:22 +0100

    category search

--- src/routes/placesRoute.ts
+++ src/routes/placesRoute.ts
@@ -23,7 +23,7 @@ router.get('/places', async (req: Request, res: Response) => {
     }
 });
 
-/// Search API endpoint
+// Search API endpoint
 router.get('/search', async (req: Request, res: Response) => {
     try {
         const { category, query } = req.query;
@@ -32,29 +32,64 @@ router.get('/search', async (req: Request, res: Response) => {
             return res.status(400).json({ message: 'Category and query parameters are required.' });
         }
 
+        // Adjust the query to allow for optional whitespace between characters
+        const adjustedQuery = (query as string).split('').join('\\s*');
+
         let searchResults: any[] = [];
 
-        // Perform the search based on the provided category and query
+        // Perform the search based on the provided category and adjusted query
         if (category === 'region') {
             searchResults = await Place.aggregate([
-                { $match: { name: { $regex: new RegExp(query as string, 'i') } } },
+                { $match: { name: { $regex: new RegExp(adjustedQuery, 'i') } } },
                 { $unwind: '$states' },
-                { $group: { _id: '$name', states: { $push: '$states.name' } } }
+                { $group: { _id: '$name', states: { $push: '$states.name' } } },
+                { $project: { region: '$_id', states: 1, _id: 0 } } 
             ]);
         } else if (category === 'state') {
             searchResults = await Place.aggregate([
                 { $unwind: '$states' },
-                { $match: { 'states.name': { $regex: new RegExp(query as string, 'i') } } },
-                { $group: { _id: '$name', states: { $push: '$states.name' } } }
+                { $match: { 'states.name': { $regex: new RegExp(adjustedQuery, 'i') } } },
+                { 
+                    $group: { 
+                        _id: '$name', 
+                        states: { 
+                            $push: { 
+                                name: '$states.name', 
+                                lgas: '$states.lgas.name' 
+                            } 
+                        } 
+                    } 
+                },
+                { 
+                    $project: { 
+                        _id: 0, 
+                        region: '$_id', 
+                        states: 1 
+                    }
+                }
             ]);
         } else if (category === 'lga') {
-            searchResults = await Place.find({
-                'states.lgas.name': { $regex: new RegExp(query as string, 'i') }
-            }, { 'states.lgas.$': 1 });
+            searchResults = await Place.aggregate([
+                { $unwind: '$states' },
+                { $unwind: '$states.lgas' },
+                { $match: { 'states.lgas.name': { $regex: new RegExp(adjustedQuery, 'i') } } },
+                {
+                    $project: {
+                        _id: 0,
+                        state: '$states.name',
+                        lga: '$states.lgas.name',
+                        metadata: '$states.lgas.metadata'
+                    }
+                }
+            ]);
         } else {
             return res.status(400).json({ message: 'Invalid category parameter.' });
         }
 
+             // Return a message if no results are found
+        if (searchResults.length === 0) {
+            return res.status(404).json({ message: `No results found for query '${query}' in the '${category}' category.` });
+        }
         // Return the search results
         res.json(searchResults);
     } catch (error) {
@@ -62,6 +97,7 @@ router.get('/search', async (req: Request, res: Response) => {
         res.status(500).json({ message: 'Internal server error.' });
     }
 });
+
 
 
 export default router;
